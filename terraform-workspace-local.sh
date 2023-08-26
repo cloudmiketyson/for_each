681528ee9d1b68427a0426dc00b54a5f6644ab13
cloudmiketyson <134102900+cloudmiketyson@users.noreply.github.com>
2023-08-25 22:53:58 -0400

    Update terraform-workspace-local.sh

--- terraform-workspace-local.sh
+++ terraform-workspace-local.sh
@@ -7,7 +7,7 @@
 ##########################################################
 set -e
 
-organization=${TF_ORGANIZATION:=" thiesco-DevOps"}
+organization=${TF_ORGANIZATION:="thiesco-DevOps"}
 terraformapiurl=${TF_API_URL:="https://app.terraform.io/api/v2"}
 terraformversion=${TF_VERSION:=1.0.0}
 AGENT_HOMEDIRECTORY=${AGENT_HOMEDIRECTORY:="$HOME"}
